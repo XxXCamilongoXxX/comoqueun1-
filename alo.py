0fa26a5ea046e2adc35e435e317937a08e7459f6
warencigarro <jos.quezadar@duocuc.cl>
2024-06-12 10:25:41 -0400

    dios me abandono

---
+++ alo.py
@@ -0,0 +1,8 @@
+#pq ensayo sorpresa :,v
+
+personas =[["nombre","cargo","sueldo"]]
+while True:
+    print("1.Registrar trabajador\n2.Lista de trabajadores\n3.Imprimir planilla de sueldos\n4.Salir")
+    opc = int(input("Eliga una opcion: "))
+    
+    
